6bac0de0ebfa089b6c7b229dca09d5987932fbf8
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-04-06 22:20:57 +0530

    learning game dev

---
+++ pygame-practice/code_1.py
@@ -0,0 +1,14 @@
+import pygame
+
+#setup pygame
+pygame.init()
+screen=pygame.display.set_mode((1280,700))
+clock=pygame.time.Clock(  )
+running =True
+
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+
+    screen.f
