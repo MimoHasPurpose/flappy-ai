e924dd45303c3c862153ff0c8b6cea83618b3c3e
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-04-07 15:54:11 +0530

    updated with a circle which moves

--- pygame-practice/code_1.py
+++ pygame-practice/code_1.py
@@ -2,17 +2,23 @@ import pygame
 
 #setup pygame
 pygame.init()
+screen=pygame.display.set_mode((550,550))
 pygame.font.init()
-pygame.display.set_caption("Sammie or Samantha!!")
+pygame.display.set_caption("Sammie vs Mimo!!")
 
-clock=pygame.time.Clock(  )
+clock=pygame.time.Clock()
 running =True
-
+i=0
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-   
+    #game starts from here!
+
+    screen.fill("purple")
+
+    pygame.draw.circle(screen,"red",(200+i,200),40)
+    i=i+1
 
     pygame.display.flip()
     clock.tick(60)
